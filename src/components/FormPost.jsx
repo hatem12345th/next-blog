5c9b3fe195b89e3ec03d7f22530bfcb7c66c4164
hatem12345th <h.taleb@esi-sba.dz>
2024-08-25 21:57:16 +0100

    Create PostFrom

--- src/components/FormPost.jsx
+++ src/components/FormPost.jsx
@@ -1,6 +1,15 @@
+"use client"
+
 import Link from "next/link"
+import { useForm } from "react-hook-form"
+
 
 export const FormPost = () => {
+  const {register,handleSubmit} = useForm();
+  const onSubmit = (d) =>   {
+    alert(JSON.stringify(d))
+  }
+
   return (
     <div className=" px-[260px]   ">
 
@@ -10,15 +19,26 @@ export const FormPost = () => {
       >  
          <span> {"< "} </span> Back
       </Link>
-
+      <form onSubmit={handleSubmit(onSubmit)}  
+    className=""
+  >
       <div className=" w-full flex  flex-col  justify-center items-center mt-10 ">
         
         <h1 className="text-3xl  font-semibold "> Add  new post  </h1>
 
-        <input type="text " placeholder="Post title " className="mt-10 px-10 border-black input w-full input-bordered max-w-lg " />
-        <textarea className="textarea textarea-bordered mt-6 border-black max-w-lg w-full " placeholder="Bio"></textarea>
 
-        <select className="select select-bordered   w-full max-w-lg border-black mt-10 ">
+        <input 
+        type="text "
+         placeholder="Post title "
+        {...register("title")}
+        className="mt-10 px-10 border-black input w-full input-bordered max-w-lg " />
+        <textarea className="textarea textarea-bordered mt-6 border-black max-w-lg w-full " placeholder="Bio"
+          {...register("bio")}
+       > fff</textarea>
+
+        <select className="select select-bordered   w-full max-w-lg border-black mt-10 "
+          {...register("tag")}
+        >
   <option disabled selected className="bg-[#E4E4E4]">  Select tag</option>
   <option  className="bg-[#E4E4E4]" >  Javascript </option>
   <option  className="bg-[#E4E4E4]" >  Python  </option>
@@ -29,6 +49,7 @@ export const FormPost = () => {
 
 
       </div>
+      </form>
 
 
 
